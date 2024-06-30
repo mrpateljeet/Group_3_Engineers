c57fc2b00e00052709a366890d74cc5b54a251a3
BollepalliPranitha <60147932+BollepalliPranitha@users.noreply.github.com>
2024-06-30 07:29:10 -0500

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('sql5715513', 'sql5715513', 'IrHV9pNJ6E', {
-    host: 'sql5.freesqldatabase.com',
+const sequelize = new Sequelize('freedb_sql5715513', 'freedb_sql5715513', 'Y3uSA4c#Mn2n77z', {
+    host: 'sql.freedb.tech',
     dialect: 'mysql',
 });
 
